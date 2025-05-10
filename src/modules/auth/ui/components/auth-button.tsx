a48cd775e7691973580f7e92335a93fd2869ad21
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2025-05-11 02:30:56 +0700

    auth(auth-button): make auth button work

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,3 +1,5 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
 import { UserCircleIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -5,12 +7,22 @@ import { Button } from '@/components/ui/button';
 export const AuthButton = () => {
 	// TODO: add different auth state
 	return (
-		<Button
-			variant="outline"
-			className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
-		>
-			<UserCircleIcon />
-			Sign in
-		</Button>
+		<>
+			<SignedIn>
+				<UserButton />
+				{/* TODO: add menu items for studio and user profile */}
+			</SignedIn>
+			<SignedOut>
+				<SignInButton mode="modal">
+					<Button
+						variant="outline"
+						className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+					>
+						<UserCircleIcon />
+						Sign in
+					</Button>
+				</SignInButton>
+			</SignedOut>
+		</>
 	);
 };
