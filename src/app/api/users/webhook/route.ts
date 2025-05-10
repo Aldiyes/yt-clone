a0e7fcc1e3756198b90ace113d414f9a94519a70
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2025-05-11 04:12:56 +0700

    webhook sync with database

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,16 +1,42 @@
+import { db } from '@/db';
+import { users } from '@/db/schema';
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
+import { eq } from 'drizzle-orm';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
 	try {
 		const evt = await verifyWebhook(req);
 
-		const { id } = evt.data;
 		const eventType = evt.type;
-		console.log(
-			`Received webhook with ID ${id} and event type of ${eventType}`
-		);
-		console.log('Webhook payload:', evt.data);
+
+		if (eventType === 'user.created') {
+			const data = evt.data;
+			await db.insert(users).values({
+				clerkId: data.id,
+				name: `${data.first_name} ${data.last_name}`,
+				imageUrl: data.image_url,
+			});
+		}
+
+		if (eventType === 'user.deleted') {
+			const { data } = evt;
+			if (!data.id) {
+				return NextResponse.json('Missing user id', { status: 400 });
+			}
+			await db.delete(users).where(eq(users.clerkId, data.id));
+		}
+
+		if (eventType === 'user.updated') {
+			const { data } = evt;
+			await db
+				.update(users)
+				.set({
+					name: `${data.first_name} ${data.last_name}`,
+					imageUrl: data.image_url,
+				})
+				.where(eq(users.clerkId, data.id));
+		}
 
 		return new NextResponse('Webhook received', { status: 200 });
 	} catch (err) {
